32b8d3eeb724f2b47e835b6becfa9a4a7cb62d03
Laila Case <lai_case@hotmail.com>
2023-10-30 14:47:23 +0000

    complete request with dynamic query endpoint

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ require("dotenv").config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY);
-const endpoint =
-  "https://api.giphy.com/v1/gifs/search?api_key=HfJMJ4e9jJMvfWD56imyt6h5w7LYiWG8&q=confused&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=confused&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`;
+
+async function getImages(query) {
+  try {
+    const request = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+    );
+    const response = await request.json();
+    if (response.data && response.data.length > 0) {
+      console.log(response.data[0].url);
+      const imgUrl = response.data[0].url;
+      return imgUrl;
+    } else {
+      console.log("no images found");
+    }
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+getImages("success");
