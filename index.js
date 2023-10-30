a238bd9216a4995f5c575ce91ae6d5138636050a
Laila Case <lai_case@hotmail.com>
2023-10-30 14:20:07 +0000

    set up endpoint and api

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+console.log(process.env.API_KEY);
+const endpoint =
+  "https://api.giphy.com/v1/gifs/search?api_key=HfJMJ4e9jJMvfWD56imyt6h5w7LYiWG8&q=confused&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
